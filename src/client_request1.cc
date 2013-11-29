a76c21813ea2cd40334c888bec3efe0cf636c18b
Alex Sandro Garzão <alex.garzao@azion.com>
2013-11-29 14:30:01 -0200

    Ajustes na formatacao do codigo.
    Ticket #1.

--- src/client_request1.cc
+++ src/client_request1.cc
@@ -7,41 +7,38 @@
 
 int main()
 {
-int sd;
-struct sockaddr_in addr;
-// int addr_len = sizeof(addr);
-char buffer[BUFFER_SIZE] = "";
+    int sd;
+    struct sockaddr_in addr;
+    // int addr_len = sizeof(addr);
+    char buffer[BUFFER_SIZE] = "";
 
-// Create client socket
-if( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
-{
-  perror("socket error");
-  return -1;
-}
+    // Create client socket
+    if( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 ) {
+        perror("socket error");
+        return -1;
+    }
 
-bzero(&addr, sizeof(addr));
+    bzero(&addr, sizeof(addr));
 
-addr.sin_family = AF_INET;
-addr.sin_port = htons(PORT_NO);
-addr.sin_addr.s_addr =  htonl(INADDR_ANY);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT_NO);
+    addr.sin_addr.s_addr =  htonl(INADDR_ANY);
 
-// Connect to server socket
-if(connect(sd, (struct sockaddr *)&addr, sizeof addr) < 0)
-{
-  perror("Connect error");
-  return -1;
-}
+    // Connect to server socket
+    if(connect(sd, (struct sockaddr *)&addr, sizeof addr) < 0) {
+        perror("Connect error");
+        return -1;
+    }
 
-while (strcmp(buffer, "q") != 0)
-{
-  // Read input from user and send message to the server
-  gets(buffer);
-  send(sd, buffer, strlen(buffer), 0);
+    while (strcmp(buffer, "q") != 0) {
+        // Read input from user and send message to the server
+        gets(buffer);
+        send(sd, buffer, strlen(buffer), 0);
 
-  // Receive message from the server
-  recv(sd, buffer, BUFFER_SIZE, 0);
-  printf("message: %s\n", buffer);
-}
+        // Receive message from the server
+        recv(sd, buffer, BUFFER_SIZE, 0);
+        printf("message: %s\n", buffer);
+    }
 
-return 0;
+    return 0;
 }
