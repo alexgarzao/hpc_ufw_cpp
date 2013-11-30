ab900d82f3113325d697305b100a04cd8f085508
Alex Garzao <alexgarzao@gmail.com>
2013-11-30 20:35:36 -0200

    Movendo o arquivo para o local correto.
    Issue #1.

--- comp.sh
+++ comp.sh
@@ -1,2 +1,2 @@
-g++ -W -Wall -Wextra -Isrc/ -Itests/ tests/server1.cc src/concurrency_model.cc src/standard_socket_interface.cc src/ufw.cc src/multiply_numbers.cc -lev -o bin/server1 && \
+g++ -W -Wall -Wextra -Isrc/ tests/server1.cc src/concurrency_model.cc src/standard_socket_interface.cc src/ufw.cc tests/multiply_numbers.cc -lev -o bin/server1 && \
 g++ -W -Wall -Wextra -Isrc/ -Itests/ tests/client_request1.cc -o bin/cr1
