0ffd1fbb38aadb03fd787735cb6fffe3ec179e58
Alex Sandro Garzão <alex.garzao@azion.com>
2013-12-03 08:05:05 -0200

    Corrigindo erro no ultimo merge.
    Issue #1.

--- tests/client_request1.cc
+++ tests/client_request1.cc
@@ -2,17 +2,6 @@
 #include <strings.h>
 #include <netinet/in.h>
 
-<<<<<<< HEAD
-#define PORT_NO 9999
-#define BUFFER_SIZE 1024
-
-int main()
-{
-    int sd;
-    struct sockaddr_in addr;
-    // int addr_len = sizeof(addr);
-    char buffer[BUFFER_SIZE] = "";
-=======
 #include "protocol1.h"
 
 #define PORT_NO     9999
@@ -26,7 +15,6 @@ main()
     struct sockaddr_in  addr;
     message_request_t   message_request;
     message_reply_t     message_reply;
->>>>>>> 13b4d034d6dd14d94a8b1e57033e49b817ab8371
 
     // Create client socket
     if( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 ) {
@@ -36,15 +24,9 @@ main()
 
     bzero(&addr, sizeof(addr));
 
-<<<<<<< HEAD
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT_NO);
-    addr.sin_addr.s_addr =  htonl(INADDR_ANY);
-=======
     addr.sin_family         = AF_INET;
     addr.sin_port           = htons(PORT_NO);
     addr.sin_addr.s_addr    =  htonl(INADDR_ANY);
->>>>>>> 13b4d034d6dd14d94a8b1e57033e49b817ab8371
 
     // Connect to server socket
     if(connect(sd, (struct sockaddr *)&addr, sizeof addr) < 0) {
@@ -52,17 +34,6 @@ main()
         return -1;
     }
 
-<<<<<<< HEAD
-    while (strcmp(buffer, "q") != 0) {
-        // Read input from user and send message to the server
-        gets(buffer);
-        send(sd, buffer, strlen(buffer), 0);
-
-        // Receive message from the server
-        recv(sd, buffer, BUFFER_SIZE, 0);
-        printf("message: %s\n", buffer);
-    }
-=======
     message_request.value1 = 10;
     message_request.value2 = 20;
 
@@ -81,7 +52,6 @@ main()
     //     recv(sd, buffer, BUFFER_SIZE, 0);
     //     printf("message: %s\n", buffer);
     // }
->>>>>>> 13b4d034d6dd14d94a8b1e57033e49b817ab8371
 
     return 0;
 }
